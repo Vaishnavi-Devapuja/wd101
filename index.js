f06c130bafe40b9c832efd5612f337a6bbedacc6
Vaishnavi-Devapuja <141746248+Vaishnavi-Devapuja@users.noreply.github.com>
2023-08-08 21:09:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,80 @@
-const userForm=document.getElementById('user-form');
+const form=document.getElementById('user-form');
 const entriesTable=document.getElementById('entries-body');
 const emailInput=document.getElementById('email');
 const nameInput=document.getElementById('name');
+const dobInput=document.getElementById('dob');
 
-nameInput.addEventListener('input',function() {
-    if (!/^[A-Za-z\s]+$/.test(nameInput.value)) {
+emailInput.addEventListener('input',function () {
+    emailInput.setCustomValidity('');
+});
+
+function isValidEmail(email) {
+    const emailRegex=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+form.addEventListener('submit',function(event) {
+    event.preventDefault();
+
+    const name=nameInput.value;
+    const email=emailInput.value;
+    const password=document.getElementById('password').value;
+    const dob=dobInput.value;
+    const acceptTerms=document.getElementById('acceptTerms').checked;
+
+    if (!/^[A-Za-z\s]+$/.test(name)) {
         nameInput.setCustomValidity('Name should contain only alphabets and space characters');
         nameInput.reportValidity();
+        return;
     } else {
         nameInput.setCustomValidity('');
     }
+
+    if (!isValidEmail(email)) {
+        emailInput.setCustomValidity('Invalid email format');
+        emailInput.reportValidity();
+        return;
+    } else {
+        emailInput.setCustomValidity('');
+    }
+
+    const currentDate=new Date();
+    const dobDate=new Date(dob);
+    const age=currentDate.getFullYear() - dobDate.getFullYear();
+
+    if (age<18||age>55) {
+        dobInput.setCustomValidity('Age should be between 18 and 55.');
+        dobInput.reportValidity();
+        return;
+    } else {
+        dobInput.setCustomValidity('');
+    }
+
+    const userData={ name, email, password, dob, acceptTerms };
+    let storedEntries=JSON.parse(localStorage.getItem('userData')) || [];
+    storedEntries.push(userData);
+    localStorage.setItem('userData', JSON.stringify(storedEntries));
+
+    updateEntriesTable();
+
+    form.reset();
 });
 
-userForm.addEventListener('submit',function(event) {
-    event.preventDefault();
-
-    const name=document.getElementById('name').value;
-    const email=emailInput.value;
-    const password=document.getElementById('password').value;
-    const dob=document.getElementById('dob').value;
-    const acceptTerms=document.getElementById('acceptTerms').checked;
-    const userData={name, email, password, dob, acceptTerms};
-    localStorage.setItem('userData', JSON.stringify(userData));
-
-    updateEntriesTable(userData);
-
-    userForm.reset();
-});
-
-function updateEntriesTable(userData) {
-    const newRow=entriesTable.insertRow();
-    newRow.innerHTML=`
-        <td>${userData.name}</td>
-        <td>${userData.email}</td>
-        <td>${userData.password}</td>
-        <td>${userData.dob}</td>
-        <td>${userData.acceptTerms ? 'Yes' : 'No'}</td>
-    `;
+function updateEntriesTable() {
+    entriesTable.innerHTML = '';
+    let storedEntries = JSON.parse(localStorage.getItem('userData')) || [];
+    storedEntries.forEach(userData => {
+        const newRow = entriesTable.insertRow();
+        newRow.innerHTML = `
+            <td>${userData.name}</td>
+            <td>${userData.email}</td>
+            <td>${userData.password}</td>
+            <td>${userData.dob}</td>
+            <td>${userData.acceptTerms ? 'Yes' : 'No'}</td>
+        `;
+    });
 }
 
 window.addEventListener('load',function() {
-    const userDataJSON=localStorage.getItem('userData');
-    if (userDataJSON) {
-        const userData=JSON.parse(userDataJSON);
-        updateEntriesTable(userData);
-    }
-});
-window.addEventListener('load',function() {
-    entriesTable.innerHTML='';
+    updateEntriesTable();
 });
